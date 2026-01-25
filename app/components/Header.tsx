2945a37f99cb18f3f6e42416edeffd33e42a5f04
holyhw <hyunookoo22@gmail.com>
2026-01-25 17:56:52 +0900

    feat: 헤더 마크업 및 디자인 반응형 구현
    추가적으로 데스크탑 버전부터 작업해서 반응형구현할때 모바일 퍼스트로 코드 리팩토링함. 추후 모바일버전일때 햄버거버튼 구현할 예정임.

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -5,49 +5,73 @@ import styles from './Header.module.css';
 export default function Header() {
   const isLogin = true;
   return (
-    <header className={styles[`header-wrapper`]}>
-      <div className={styles[`logo-wrapper`]}>
-        <Link href="/">
-          <Image src="./logo/logo.svg" width={79} height={69} alt="로고 이미지" />
-        </Link>
-      </div>
-      <div className={styles[`meetings-wrapper`]}>
-        <ul>
-          <li>
-            <Link href="/meetings">리스트</Link>
-          </li>
-          <li>
-            <Link href="/bookmarks">북마크</Link>
-          </li>
-          <li>
-            <Link href="/history">모임 조회</Link>
-          </li>
-        </ul>
-      </div>
-      {isLogin ? (
-        <div className={styles[`user-info-wrapper`]}>
+    <>
+      <header className={styles[`header-wrapper`]}>
+        <div className={styles[`logo-wrapper`]}>
+          <Link href="/">
+            <Image src="./logo/logo.svg" width={40} height={35} alt="로고 이미지" />
+          </Link>
+        </div>
+
+        {/* 데스크탑 메뉴 */}
+        <div className={styles[`meetings-wrapper`]}>
           <ul>
             <li>
-              <button>로그아웃</button>
+              <Link href="/meetings">리스트</Link>
             </li>
             <li>
-              <Link href="/notifications">
-                <Image src="./icon/notification.svg" width={43} height={50} alt="알림 이미지" />
-              </Link>
+              <Link href="/bookmarks">북마크</Link>
             </li>
-            <li className={styles[`image-background`]}>
-              <Link className={styles[`profile-image-wrapper`]} href="/mypage">
-                <Image src="./icon/profile.svg" width={41} height={41} alt="사용자 디폴트 이미지" />
-              </Link>
+            <li>
+              <Link href="/history">모임 조회</Link>
             </li>
           </ul>
         </div>
-      ) : (
-        <div className={styles[`sign-wrapper`]}>
-          <Link href="/login">로그인</Link>
-          <Link href="/signup">회원가입</Link>
+
+        {/* 데스크탑 유저 정보 */}
+        {isLogin ? (
+          <div className={styles[`user-info-wrapper`]}>
+            <ul>
+              <li>
+                <button>로그아웃</button>
+              </li>
+              <li>
+                <Link href="/notifications">
+                  <Image src="./icon/notification.svg" width={43} height={50} alt="알림 이미지" />
+                </Link>
+              </li>
+              <li>
+                <Link href="/mypage">
+                  <div className={styles[`image-background`]}>
+                    <Image src="./icon/profile.svg" width={41} height={41} alt="사용자 디폴트 이미지" />
+                  </div>
+                </Link>
+              </li>
+            </ul>
+          </div>
+        ) : (
+          <div className={styles[`sign-wrapper`]}>
+            <ul>
+              <li>
+                <Link href="/login">로그인</Link>
+              </li>
+              <li>
+                <Link href="/signup">회원가입</Link>
+              </li>
+            </ul>
+          </div>
+        )}
+
+        {/* 모바일 메뉴 */}
+        <div className={styles[`mobile-menu`]}>
+          <Link href="/notifications">
+            <Image src="./icon/notification.svg" width={25} height={30} alt="알림 이미지" />
+          </Link>
+          <button className={styles[`hamburger-btn`]}>
+            <Image src="./icon/hamburger.svg" width={30} height={28} alt="메뉴" />
+          </button>
         </div>
-      )}
-    </header>
+      </header>
+    </>
   );
 }
