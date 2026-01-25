4d2a1c7ab20d5915ea9e20263e75d505a297ba6e
minggichae <che08180@naver.com>
2026-01-25 20:35:29 +0900

    feat: 모임 신청 기초 마크업 작성 #17

--- app/meetings/[id]/apply/page.tsx
+++ app/meetings/[id]/apply/page.tsx
@@ -1,3 +1,20 @@
 export default function Apply() {
-  return <h1>apply</h1>;
+  return (
+    <>
+      <h1>모임 신청</h1>
+      <div>
+        <h3>질문 1</h3>
+        <textarea name="질문 1" id="" style={{ backgroundColor: '#c2c2c2' }}></textarea>
+      </div>
+      <div>
+        <h3>질문 2</h3>
+        <textarea name="질문 2" id="" style={{ backgroundColor: '#c2c2c2' }}></textarea>
+      </div>
+      <div>
+        <button>신청하기</button>
+        <br />
+        <button>취소하기</button>
+      </div>
+    </>
+  );
 }
