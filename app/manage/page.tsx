b9f8c95751d81b1402188c9bf61dea37fd903b16
minggichae <che08180@naver.com>
2026-01-25 20:40:58 +0900

    feat: 참여자 관리 기초 마크업 작성 #16

--- app/manage/page.tsx
+++ app/manage/page.tsx
@@ -1,3 +1,30 @@
+import profile from '@/public/icon/profile.svg';
+import heart from '@/public/icon/heart.svg';
+
 export default function Manage() {
-  return <h1>Manage</h1>;
+  return (
+    <>
+      <h1>신청자 목록</h1>
+      <div>
+        <img src={profile.src} alt="프로필이미지" />
+        <div>
+          <span>나폴리맛피자</span>
+          <figure>
+            <img src={heart.src} alt="심박수이미지" />
+            <figcaption>90bpm</figcaption>
+          </figure>
+          <button>승인</button>
+          <button>거절</button>
+        </div>
+        <div>
+          <h3>질문1</h3>
+          <p>질문에 대한 답변을 가져오는 위치</p>
+        </div>
+        <div>
+          <h3>질문2</h3>
+          <p>질문에 대한 답변을 가져오는 위치</p>
+        </div>
+      </div>
+    </>
+  );
 }
