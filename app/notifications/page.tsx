d4da4e4f04e456cb5a5c5086f024b8f8e7f6a0e9
jian526 <catthsudla@gmail.com>
2026-01-28 09:01:02 +0900

    feat: 샘플 이미지 추가

--- app/notifications/page.tsx
+++ app/notifications/page.tsx
@@ -2,7 +2,7 @@
 
 import Image from 'next/image';
 import Link from 'next/link';
-import img from './sampleImg.jpg';
+import img from './sampleImg.png';
 import DefaultLayout from '@/app/components/DefaultLayout';
 import styles from './notifications.module.css';
 
