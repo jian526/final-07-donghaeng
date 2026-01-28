e7f3b81447adad6d17b5048e3bd196dc5cda828c
holyhw <hyunookoo22@gmail.com>
2026-01-28 22:47:21 +0900

    feat: 모임 목록, 상세 조회 api 설정

---
+++ lib/meetings.ts
@@ -0,0 +1,51 @@
+import { ErrorRes, MeetingsInfoRes, MeetingsListRes } from '@/types/api';
+
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
+const CLIENT_ID = process.env.NEXT_PUBLIC_CLIENT_ID || '';
+
+/**
+ * 게시판 타입에 해당하는 모임 목록 조회
+ * @returns {Promise<MeetingsListRes | ErrorRes>} - 모임 목록 응답 객체
+ */
+export async function getMeetings(): Promise<MeetingsListRes | ErrorRes> {
+  try {
+    const res = await fetch(`${API_URL}/products`, {
+      headers: {
+        'Client-Id': CLIENT_ID,
+      },
+      cache: 'force-cache',
+      next: {
+        tags: [`products`],
+      },
+    });
+    return res.json();
+  } catch (error) {
+    // 네트워크 오류 처리
+    console.error(error);
+    return { ok: 0, message: '일시적인 네트워크 문제로 게시물 목록 조회에 실패했습니다.' };
+  }
+}
+
+/**
+ * 특정 모임의 상세 정보 조회
+ * @param {string} _id - 모임 ID
+ * @returns {Promise<MeetingsInfoRes | ErrorRes>} - 모임 상세 정보 응답 객체
+ */
+export async function getDetail(_id: string): Promise<MeetingsInfoRes | ErrorRes> {
+  try {
+    const res = await fetch(`${API_URL}/products/${_id}`, {
+      headers: {
+        'Client-Id': CLIENT_ID,
+      },
+      cache: 'force-cache',
+      next: {
+        tags: [`products/${_id}`],
+      },
+    });
+    return res.json();
+  } catch (error) {
+    // 네트워크 오류 처리
+    console.error(error);
+    return { ok: 0, message: '일시적인 네트워크 문제로 게시물 상세 조회에 실패했습니다.' };
+  }
+}
