0a52015cd23cbebc8b3e704fd711944d802413ee
minggichae <che08180@naver.com>
2026-01-29 16:42:17 +0900

    feat: 카카오맵 api를 통한 마커 및 오버레이 기능 추가 #6 style: 전체적인 지도 스타일 변경 modify: 코드 구조 수정

--- app/map/page.tsx
+++ app/map/page.tsx
@@ -1,7 +1,6 @@
-import dobai from './images/doobai.png';
+'use client';
+
 import tag from '@/public/icon/tag.svg';
-import info from '@/public/icon/info.svg';
-import people from '@/public/icon/people.svg';
 import calender from '@/public/icon/calendar.svg';
 import search from '@/public/icon/search.svg';
 import styles from './Map.module.css';
@@ -9,8 +8,23 @@ import Filter from '@/app/components/Filter';
 import DefaultLayout from '@/app/components/DefaultLayout';
 import Image from 'next/image';
 import KakaoMap from './KakaoMap';
+import { useEffect, useState } from 'react';
+import { Meetings } from '@/types/meetings';
+import { getMeetings } from '@/lib/meetings';
 
 export default function Map() {
+  const [meetings, setMeetings] = useState<Meetings[]>([]);
+
+  useEffect(() => {
+    const fetchMeetings = async () => {
+      const res = await getMeetings();
+      if (res.ok === 1) {
+        setMeetings(res.item);
+      }
+    };
+    fetchMeetings();
+  }, []);
+
   return (
     <>
       <DefaultLayout>
@@ -25,83 +39,25 @@ export default function Map() {
                 <Image src={search.src} alt="검색" width={20} height={20} />
               </div>
               <ul>
-                <li>
-                  <Image src={dobai.src} alt="모임 사진" width={90} height={80} />
-                  <div>
-                    <dt>세상에서 제일 비싼 두쫀쿠 만들기</dt>
-                    <dd>서울특별시 마포구 . 보드게임, 20대</dd>
-                  </div>
-                </li>
-                <li>
-                  <Image src={dobai.src} alt="모임 사진" width={90} height={80} />
-                  <div>
-                    <dt>세상에서 제일 비싼 두쫀쿠 만들기</dt>
-                    <dd>서울특별시 마포구 . 보드게임, 20대</dd>
-                  </div>
-                </li>
-                <li>
-                  <Image src={dobai.src} alt="모임 사진" width={90} height={80} />
-                  <div>
-                    <dt>세상에서 제일 비싼 두쫀쿠 만들기</dt>
-                    <dd>서울특별시 마포구 . 보드게임, 20대</dd>
-                  </div>
-                </li>
-                <li>
-                  <Image src={dobai.src} alt="모임 사진" width={90} height={80} />
-                  <div>
-                    <dt>세상에서 제일 비싼 두쫀쿠 만들기</dt>
-                    <dd>서울특별시 마포구 . 보드게임, 20대</dd>
-                  </div>
-                </li>
-                <li>
-                  <Image src={dobai.src} alt="모임 사진" width={90} height={80} />
-                  <div>
-                    <dt>세상에서 제일 비싼 두쫀쿠 만들기</dt>
-                    <dd>서울특별시 마포구 . 보드게임, 20대</dd>
-                  </div>
-                </li>
-                <li>
-                  <Image src={dobai.src} alt="모임 사진" width={90} height={80} />
-                  <div>
-                    <dt>세상에서 제일 비싼 두쫀쿠 만들기</dt>
-                    <dd>서울특별시 마포구 . 보드게임, 20대</dd>
-                  </div>
-                </li>
-                <li>
-                  <Image src={dobai.src} alt="모임 사진" width={90} height={80} />
-                  <div>
-                    <dt>세상에서 제일 비싼 두쫀쿠 만들기</dt>
-                    <dd>서울특별시 마포구 . 보드게임, 20대</dd>
-                  </div>
-                </li>
+                {meetings.map((meeting) => (
+                  <li key={meeting._id}>
+                    <Image src={meeting.mainImages[0].path} alt="모임 사진" width={90} height={80} />
+                    <div className={styles['meeting-info-li-div']}>
+                      <dt>{meeting.name}</dt>
+                      <dd>
+                        <Image src={tag.src} alt="태그" width={20} height={12} />
+                        {meeting.extra.region} . {meeting.extra.category}
+                      </dd>
+                      <dd>
+                        <Image src={calender.src} alt="정보" width={20} height={12} /> {meeting.extra.date}
+                      </dd>
+                    </div>
+                  </li>
+                ))}
               </ul>
             </div>
 
-            <KakaoMap width="100%" height="500px" lat={37.5709} lng={126.978} className={styles.map} />
-          </div>
-          <div className={styles['metting-div']}>
-            <div className={styles['metting-information']}>
-              <Image src={dobai.src} alt="모임 사진" width={150} height={150} className={styles['metting-information-img']} />
-              <div className={styles['detail-information']}>
-                <h3>세상에서 제일 비싼 두쫀쿠 만들기</h3>
-                <p>
-                  <Image src={tag.src} alt="태그" width={14} height={12} />
-                  마포구. 보드게임
-                </p>
-                <p>
-                  <Image src={info.src} alt="나이 성별" width={14} height={11} />
-                  20 ~ 30대, 남녀무관
-                </p>
-                <p>
-                  <Image src={people.src} alt="인원" width={14} height={11} />
-                  인원 4명 ~ 5명
-                </p>
-                <p>
-                  <Image src={calender.src} alt="캘린더" width={14} height={12} />
-                  26.1.27(화) 오후 3:00
-                </p>
-              </div>
-            </div>
+            <KakaoMap width="100%" height="700px" lat={37.5709} lng={126.978} className={styles.map} meetings={meetings} />
           </div>
         </main>
       </DefaultLayout>
