b0a36786f1a84d6d8c0b7361fddaaf67adc340ee
holyhw <hyunookoo22@gmail.com>
2026-02-02 19:02:12 +0900

    feat: 모임신청 기능 구현(WOKR IN PROGRESS)

---
+++ app/meetings/[id]/apply/ApplyForm.tsx
@@ -0,0 +1,71 @@
+'use client';
+
+import useUserStore from '@/zustand/userStore';
+import styles from './Apply.module.css';
+import { Meetings } from '@/types/meetings';
+import { createApply } from '@/actions/meetings';
+
+interface ApplyFormProps {
+  meeting: Meetings;
+  id: string;
+}
+
+export default function ApplyForm({ meeting, id }: ApplyFormProps) {
+  const user = useUserStore((state) => state.user);
+  const userId = user?._id;
+  const accessToken = user?.token?.accessToken;
+  console.log('유저아이디', userId);
+
+  const processAction = async (id: string, answer1: string, answer2: string) => {
+    const formData = new FormData();
+    formData.append('accessToken', accessToken || '');
+    formData.append(
+      'products',
+      JSON.stringify([
+        {
+          _id: Number(id),
+          quantity: 1,
+        },
+      ])
+    );
+    formData.append(
+      'extra',
+      JSON.stringify({
+        answer1,
+        answer2,
+      })
+    );
+
+    await createApply(null, formData);
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const form = e.currentTarget;
+    const answer1 = (form.elements.namedItem('answer1') as HTMLTextAreaElement).value;
+    const answer2 = (form.elements.namedItem('answer2') as HTMLTextAreaElement).value;
+    processAction(id, answer1, answer2);
+  };
+
+  return (
+    <form className={styles['apply-body']} onSubmit={handleSubmit}>
+      <h1 className={styles['apply-title']}>모임 신청</h1>
+      <div className={styles['question-div']}>
+        <h3>{meeting.extra.survey1}</h3>
+        <textarea name="answer1"></textarea>
+      </div>
+      <div className={styles['question-div']}>
+        <h3>{meeting.extra.survey2}</h3>
+        <textarea name="answer2"></textarea>
+      </div>
+      <div className={styles['btn-div']}>
+        <button type="submit" className={styles['btn-apply']}>
+          신청하기
+        </button>
+        <button type="button" className={styles['btn-cancel']}>
+          취소하기
+        </button>
+      </div>
+    </form>
+  );
+}
