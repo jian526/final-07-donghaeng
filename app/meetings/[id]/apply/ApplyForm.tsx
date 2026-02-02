d7478da04b56a428a7cbd200cf8918ba91120949
holyhw <hyunookoo22@gmail.com>
2026-02-02 20:54:12 +0900

    docs: 코드 설명 주석 추가

--- app/meetings/[id]/apply/ApplyForm.tsx
+++ app/meetings/[id]/apply/ApplyForm.tsx
@@ -11,6 +11,7 @@ interface ApplyFormProps {
 }
 
 export default function ApplyForm({ meeting, id }: ApplyFormProps) {
+  // 유저 정보 가져오기
   const user = useUserStore((state) => state.user);
   const userId = user?._id;
   const accessToken = user?.token?.accessToken;
@@ -25,7 +26,7 @@ export default function ApplyForm({ meeting, id }: ApplyFormProps) {
       JSON.stringify([
         {
           _id: Number(id),
-          quantity: 1,
+          quantity: 0,
         },
       ])
     );
@@ -41,7 +42,7 @@ export default function ApplyForm({ meeting, id }: ApplyFormProps) {
   };
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+    e.preventDefault(); // 폼 제출 시 페이지가 리로드됨을 방지
     const form = e.currentTarget;
     const answer1 = (form.elements.namedItem('answer1') as HTMLTextAreaElement).value;
     const answer2 = (form.elements.namedItem('answer2') as HTMLTextAreaElement).value;
