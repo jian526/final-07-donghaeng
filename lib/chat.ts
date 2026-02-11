b6e8eceb17b0f1e9a25a787f2943ed514f9decaf
holyhw <hyunookoo22@gmail.com>
2026-02-11 17:03:02 +0900

    fix: 채팅기능 토큰만료 개선

--- lib/chat.ts
+++ lib/chat.ts
@@ -1,16 +1,39 @@
-import { apiCall } from '@/actions/chat';
-import { ChatRoomInfoRes, ChatRoomListRes } from '@/types/api';
+import { fetchWithAuth } from '@/utils/apiWithAuth';
+import { ChatRoomInfoRes, ChatRoomListRes, ErrorRes } from '@/types/api';
+
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
+const CLIENT_ID = process.env.NEXT_PUBLIC_CLIENT_ID || '';
 
 /**
  * 내 채팅방 목록 조회
  */
-export async function getMyRooms(accessToken: string) {
-  return apiCall<ChatRoomListRes>(`/chats`, { accessToken });
+export async function getMyRooms() {
+  const res = await fetchWithAuth(`${API_URL}/chats`, {
+    requireAuth: true,
+    headers: {
+      'Content-Type': 'application/json',
+      'client-id': CLIENT_ID,
+    },
+  });
+
+  const data: ChatRoomListRes | ErrorRes = await res.json();
+  if (!data.ok) throw new Error(data.message);
+  return data.item;
 }
 
 /**
  * 채팅방 상세 조회(없을 경우 생성)
  */
-export async function getRoomInfo({ accessToken, resourceType, resourceId }: { accessToken: string; resourceType: string; resourceId: number }) {
-  return apiCall<ChatRoomInfoRes>(`/chats/${resourceType}/${resourceId}`, { accessToken });
+export async function getRoomInfo({ resourceType, resourceId }: { resourceType: string; resourceId: number }) {
+  const res = await fetchWithAuth(`${API_URL}/chats/${resourceType}/${resourceId}`, {
+    requireAuth: true,
+    headers: {
+      'Content-Type': 'application/json',
+      'client-id': CLIENT_ID,
+    },
+  });
+
+  const data: ChatRoomInfoRes | ErrorRes = await res.json();
+  if (!data.ok) throw new Error(data.message);
+  return data.item;
 }
